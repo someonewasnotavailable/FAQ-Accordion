e61775ac19f71b1ba70d9755d23a294ec39d4e34
someone <roneyodunukwe@gmail.com>
2026-06-16 11:31:59 +0100

    yes

---
+++ Desktop/roney js files/RONEY JS PRACTICE/faq/faq by me/script.js
@@ -0,0 +1,44 @@
+'use script';
+
+const drop1 = document.getElementById('dropdown12');
+const plus1img = document.getElementById('plus1img');
+const minus1mg = document.getElementById('minus1img');
+const faq1 = document.querySelector(".faq1");
+const faq1h3 = document.getElementById('faq1h3');
+const faq2 = document.querySelector(".faq2");
+const faq3 = document.querySelector(".faq3");
+const faq4 = document.querySelector(".faq4");
+
+neat =  () => {
+    faq2.classList.remove("active");
+    faq3.classList.remove("active");
+    faq4.classList.remove("active");
+    faq1.classList.toggle("active");
+}
+
+
+neat2 = () => {
+    faq1.classList.remove("active");
+    faq3.classList.remove("active");
+    faq4.classList.remove("active");
+    faq2.classList.toggle("active");
+}
+
+neat3 = () => {
+    faq1.classList.remove("active");
+    faq2.classList.remove("active");
+    faq4.classList.remove("active");
+    faq3.classList.toggle("active");
+}
+
+neat4 = () => {
+    faq1.classList.remove("active");
+    faq2.classList.remove("active");
+    faq3.classList.remove("active");
+    faq4.classList.toggle("active")
+}
+
+faq1.addEventListener("click", neat);
+faq2.addEventListener("click", neat2);
+faq3.addEventListener("click", neat3);
+faq4.addEventListener("click", neat4);
